cada4fc1bc7880b84c8ac12a1c67e5d06b2df74f
0n11san <0n11san@users.noreply.github.com>
2018-02-06 09:22:34 -0500

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,14 @@
+//look to my .env file in order to know my personal keys
 require("dotenv").config();
+
+//import keys
+
+//import required NPM packges 
+
+/*describe various commands available to user when they run NodeJS (via terminal); 
+refer to functions below which will describe in detail what is happening in each 'case' */
+ 
+
+//describe each of callback functions that will execute when aforementioned commands are run
+
+
